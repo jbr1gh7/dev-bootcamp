8908713901e070f8403581225bf3161d78925c29
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2022-04-21 14:48:44 +0100

    3 or more query working now

--- SQL scripts/queries/students_studying_3_or_more.sql
+++ SQL scripts/queries/students_studying_3_or_more.sql
@@ -3,5 +3,6 @@ FROM Student
 JOIN CourseMembership ON (CourseMembership.StudentId = Student.Id)
 JOIN Course ON (Course.Id = CourseMembership.CourseId)
 JOIN CourseSubject ON (CourseSubject.CourseId = Course.Id)
-GROUP BY SubjectId
+JOIN Subject ON (CourseSubject.SubjectId = Subject.Id)
+GROUP BY CourseSubject.CourseId, CourseMembership.StudentId
 HAVING COUNT(*) > 3
