41c3ac6f02e5eaa4cdbac710c60d978807882af4
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2022-04-25 15:40:48 +0100

    refactor

--- StudentEnrolment/StudentEnrolment/Program.cs
+++ StudentEnrolment/StudentEnrolment/Program.cs
@@ -7,7 +7,7 @@ namespace StudentEnrolment
 {
     class Program
     {
-        static public List<T> DeserializeToList<T>(string filePath)
+        static List<T> DeserializeToList<T>(string filePath)
         {
             var file = System.IO.File.ReadAllText(filePath);
 
@@ -15,6 +15,28 @@ namespace StudentEnrolment
             {
                 PropertyNameCaseInsensitive = true
             });
+        }
+
+        static string Input(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        static List<T> makeAssocFromInput<T>(string message, List<T> reference)
+        {
+            string choices = Input(message);
+            string[] choiceList = choices.Split(",");
+
+            List<T> assocList = new List<T>();
+
+            for (int i = 0; i < choiceList.Length; i++)
+            {
+                int index = Int32.Parse(choiceList[i]);
+                assocList.Add(reference[index]);
+            }
+
+            return assocList;
         }
 
         static void Main(string[] args)
@@ -25,9 +47,9 @@ namespace StudentEnrolment
             List<Student> students = DeserializeToList<Student>("C:/Users/jacobbright/Documents/GitHub/dev-bootcamp/StudentEnrolment/StudentEnrolment/Test_Data/student.json");
             List<Subject> subjects = DeserializeToList<Subject>("C:/Users/jacobbright/Documents/GitHub/dev-bootcamp/StudentEnrolment/StudentEnrolment/Test_Data/subject.json");
 
-            courses[0].CourseSubject = subjects.GetRange(0, 4);
-            courses[1].CourseSubject = subjects.GetRange(4, 3);
-            courses[2].CourseSubject = subjects.GetRange(7, 3);
+            courses[0].CourseSubject = subjects.GetRange(0, 4); //Engineering
+            courses[1].CourseSubject = subjects.GetRange(4, 3); //Chemistry
+            courses[2].CourseSubject = subjects.GetRange(7, 3); //Law
 
             courses[0].CourseMembership = students.GetRange(0, 5);
             courses[1].CourseMembership = students.GetRange(5, 5);
@@ -35,8 +57,7 @@ namespace StudentEnrolment
 
             while (!isFinished) 
             {
-                Console.WriteLine("Select a menu choice: \n1. List all students\n2. List all courses\n3. List all subjects\n4. Add a new student\n5. Add a new course\n6. Add a new subject\n7. Delete a student\n8. Delete a course\n9. Delete a subject");
-                string choice = Console.ReadLine();
+                string choice = Input("Select a menu choice: \n1. List all students\n2. List all courses\n3. List all subjects\n4. Add a new student\n5. Add a new course\n6. Add a new subject\n7. Delete a student\n8. Delete a course\n9. Delete a subject");
                 Console.WriteLine("\n");
 
                 switch (choice)
@@ -52,62 +73,28 @@ namespace StudentEnrolment
                         break;
                     case "4":
                         string studentId = Guid.NewGuid().ToString();
-
-                        Console.WriteLine("Input first name: ");
-                        string firstName = Console.ReadLine();
-
-                        Console.WriteLine("Input last name: ");
-                        string lastName = Console.ReadLine();
+                        string firstName = Input("Input first name: ");
+                        string lastName = Input("Input last name: ");
 
                         students.Add(new Student(studentId, firstName, lastName));
                         break;
                     case "5":
                         string courseId = Guid.NewGuid().ToString();
+                        string courseName = Input("Input course name: ");
+                        string courseDescription = Input("Input course description: ");
 
-                        Console.WriteLine("Input course name: ");
-                        string courseName = Console.ReadLine();
-
-                        Console.WriteLine("Input course description: ");
-                        string courseDescription = Console.ReadLine();
-
-                        Console.WriteLine("Is this course partly funded? (y/n): ");
-                        string fundChoice = Console.ReadLine();
+                        string fundChoice = Input("Is this course partly funded? (y/n): ");
                         bool isPartFunded = fundChoice.ToLower() == "y" ? true : false;
 
-                        Console.WriteLine("Which subjects are associated with this course?\nSelect using integers delimited by a comma (e.g 1,3,5,7): ");
-                        string subjectChoices = Console.ReadLine();
-                        string[] subjectChoiceList = subjectChoices.Split(",");
-
-                        List<Subject> courseSubject = new List<Subject>();
-
-                        for (int i = 0; i < subjectChoiceList.Length; i++)
-                        {
-                            int index = Int32.Parse(subjectChoiceList[i]);
-                            courseSubject.Add(subjects[index]);
-                        }
-
-                        Console.WriteLine("Which students are associated with this course?\nSelect using integers delimited by a comma (e.g 1,3,5,7): ");
-                        string studentChoices = Console.ReadLine();
-                        string[] studentChoiceList = studentChoices.Split(",");
-
-                        List<Student> courseMembership = new List<Student>();
-
-                        for (int i = 0; i < subjectChoiceList.Length; i++)
-                        {
-                            int index = Int32.Parse(studentChoiceList[i]);
-                            courseMembership.Add(students[index]);
-                        }
+                        List<Subject> courseSubject = makeAssocFromInput("Which subjects are associated with this course?\nSelect using integers delimited by a comma (e.g 1,3,5,7): ", subjects);
+                        List<Student> courseMembership = makeAssocFromInput("Which students are associated with this course?\nSelect using integers delimited by a comma (e.g 1,3,5,7): ", students);
 
                         courses.Add(new Course(courseId, courseName, courseDescription, isPartFunded, courseSubject, courseMembership));
                         break;
                     case "6":
-                        Console.WriteLine("Input subject name: ");
-                        string subjectName = Console.ReadLine();
-
-                        Console.WriteLine("Input subject description: ");
-                        string subjectDescription = Console.ReadLine();
-
                         string subjectId = Guid.NewGuid().ToString();
+                        string subjectName = Input("Input subject name: ");
+                        string subjectDescription = Input("Input subject description: ");
 
                         subjects.Add(new Subject(subjectName, subjectDescription, subjectId));
                         break;
