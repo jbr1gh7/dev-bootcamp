b33c05e9388212e3028910dc6ab6d17491471443
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2022-04-25 12:00:01 +0100

    sets relationships

--- StudentEnrolment/StudentEnrolment/Program.cs
+++ StudentEnrolment/StudentEnrolment/Program.cs
@@ -24,7 +24,18 @@ namespace StudentEnrolment
                 PropertyNameCaseInsensitive = true
             });
 
-            courses[1].CourseSubject = 
+            courses[0].CourseSubject = subjects.GetRange(0, 4);
+            courses[1].CourseSubject = subjects.GetRange(4, 3);
+            courses[2].CourseSubject = subjects.GetRange(7, 3);
+
+            courses[0].CourseMembership = students.GetRange(0, 5);
+            courses[1].CourseMembership = students.GetRange(5, 5);
+            courses[2].CourseMembership = students.GetRange(10, 5);
+
+            for (int i = 0; i < courses[2].CourseMembership.Count; i++)
+            {
+                Console.WriteLine(courses[2].CourseMembership[i].FirstName);
+            }
         }
     }
 }
