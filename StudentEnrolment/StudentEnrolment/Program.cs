ba91cdf7bd9f1db4b64ca96b24da16b2fd31a8b2
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2022-04-25 09:59:56 +0100

    reads json data into arrays

--- StudentEnrolment/StudentEnrolment/Program.cs
+++ StudentEnrolment/StudentEnrolment/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using StudentEnrolment.Models;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
 
 namespace StudentEnrolment
 {
@@ -6,7 +9,22 @@ namespace StudentEnrolment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Course> courses = JsonSerializer.Deserialize<List<Course>>(System.IO.File.ReadAllText("C:/Users/jacobbright/Documents/GitHub/dev-bootcamp/StudentEnrolment/StudentEnrolment/Test_Data/course.json"), new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            List<Student> students = JsonSerializer.Deserialize<List<Student>>(System.IO.File.ReadAllText("C:/Users/jacobbright/Documents/GitHub/dev-bootcamp/StudentEnrolment/StudentEnrolment/Test_Data/student.json"), new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            List<Subject> subjects = JsonSerializer.Deserialize<List<Subject>>(System.IO.File.ReadAllText("C:/Users/jacobbright/Documents/GitHub/dev-bootcamp/StudentEnrolment/StudentEnrolment/Test_Data/subject.json"), new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            courses[1].CourseSubject = 
         }
     }
 }
