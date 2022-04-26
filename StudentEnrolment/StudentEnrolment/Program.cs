03cf74e1ca46173d804367dfeb8f24732197fd6d
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2022-04-26 10:20:43 +0100

    refactor

--- StudentEnrolment/StudentEnrolment/Program.cs
+++ StudentEnrolment/StudentEnrolment/Program.cs
@@ -1,12 +1,36 @@
 ﻿using StudentEnrolment.Models;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Text.Json;
 
 namespace StudentEnrolment
 {
     class Program
     {
+        static object LoadDataInProgram()
+        {
+            List<Course> courses = DeserializeToList<Course>("C:/Users/jacobbright/Documents/GitHub/dev-bootcamp/StudentEnrolment/StudentEnrolment/Test_Data/course.json");
+            List<Student> students = DeserializeToList<Student>("C:/Users/jacobbright/Documents/GitHub/dev-bootcamp/StudentEnrolment/StudentEnrolment/Test_Data/student.json");
+            List<Subject> subjects = DeserializeToList<Subject>("C:/Users/jacobbright/Documents/GitHub/dev-bootcamp/StudentEnrolment/StudentEnrolment/Test_Data/subject.json");
+
+            courses[0].CourseSubject = subjects.GetRange(0, 4); //Engineering
+            courses[1].CourseSubject = subjects.GetRange(4, 3); //Chemistry
+            courses[2].CourseSubject = subjects.GetRange(7, 3); //Law
+
+            courses[0].CourseMembership = students.GetRange(0, 5); //first 5
+            courses[1].CourseMembership = students.GetRange(5, 5); //next 5
+            courses[2].CourseMembership = students.GetRange(10, 5); //last 5
+
+            dynamic data = new ExpandoObject();
+
+            data.Courses = courses;
+            data.Students = students;
+            data.Subjects = subjects;
+
+            return data;
+        }
+
         static List<T> DeserializeToList<T>(string filePath)
         {
             var file = System.IO.File.ReadAllText(filePath);
@@ -17,14 +41,29 @@ namespace StudentEnrolment
             });
         }
 
+        static void ListStudents(List<Student> students) 
+        {
+            for (int i = 0; i < students.Count; i++) { Console.WriteLine(i.ToString() + ". " + students[i].FirstName + " " + students[i].LastName); }
+        }
+
+        static void ListCurriculum<T>(List<T> curricula)
+        where T : Curriculum
+        {
+            for (int i = 0; i < curricula.Count; i++) { Console.WriteLine(i.ToString() + ". " + curricula[i].Name + ":\n" + curricula[i].Description + "\n"); }
+        }
+
         static string Input(string message)
         {
             Console.WriteLine(message);
             return Console.ReadLine();
         }
 
-        static List<T> makeAssocFromInput<T>(string message, List<T> reference)
+        static List<T> MakeAssocFromInput<T>(string message, dynamic data, bool isSubject)
         {
+
+            if (isSubject) { ListCurriculum(data.Subjects); }
+            else { ListStudents(data.Students); }
+            
             string choices = Input(message);
             string[] choiceList = choices.Split(",");
 
@@ -33,7 +72,7 @@ namespace StudentEnrolment
             for (int i = 0; i < choiceList.Length; i++)
             {
                 int index = Int32.Parse(choiceList[i]);
-                assocList.Add(reference[index]);
+                //assocList.Add(reference[index]);
             }
 
             return assocList;
@@ -42,18 +81,7 @@ namespace StudentEnrolment
         static void Main(string[] args)
         {
             bool isFinished = false;
-
-            List<Course> courses = DeserializeToList<Course>("C:/Users/jacobbright/Documents/GitHub/dev-bootcamp/StudentEnrolment/StudentEnrolment/Test_Data/course.json");
-            List<Student> students = DeserializeToList<Student>("C:/Users/jacobbright/Documents/GitHub/dev-bootcamp/StudentEnrolment/StudentEnrolment/Test_Data/student.json");
-            List<Subject> subjects = DeserializeToList<Subject>("C:/Users/jacobbright/Documents/GitHub/dev-bootcamp/StudentEnrolment/StudentEnrolment/Test_Data/subject.json");
-
-            courses[0].CourseSubject = subjects.GetRange(0, 4); //Engineering
-            courses[1].CourseSubject = subjects.GetRange(4, 3); //Chemistry
-            courses[2].CourseSubject = subjects.GetRange(7, 3); //Law
-
-            courses[0].CourseMembership = students.GetRange(0, 5);
-            courses[1].CourseMembership = students.GetRange(5, 5);
-            courses[2].CourseMembership = students.GetRange(10, 5);
+            dynamic data = LoadDataInProgram();
 
             while (!isFinished) 
             {
@@ -63,20 +91,20 @@ namespace StudentEnrolment
                 switch (choice)
                 {
                     case "1":
-                        for (int i = 0; i < students.Count; i++) { Console.WriteLine(students[i].FirstName + " " + students[i].LastName); }
+                        ListStudents(data.Students);
                         break;
                     case "2":
-                        for (int i = 0; i < courses.Count; i++) { Console.WriteLine(courses[i].Name + ":\n" + courses[i].Description + "\n"); }
+                        ListCurriculum(data.Courses);
                         break;
                     case "3":
-                        for (int i = 0; i < subjects.Count; i++) { Console.WriteLine(subjects[i].Name + ":\n" + subjects[i].Description + "\n"); }
+                        ListCurriculum(data.Subjects);
                         break;
                     case "4":
                         string studentId = Guid.NewGuid().ToString();
                         string firstName = Input("Input first name: ");
                         string lastName = Input("Input last name: ");
 
-                        students.Add(new Student(studentId, firstName, lastName));
+                        data.Students.Add(new Student(studentId, firstName, lastName));
                         break;
                     case "5":
                         string courseId = Guid.NewGuid().ToString();
@@ -86,17 +114,17 @@ namespace StudentEnrolment
                         string fundChoice = Input("Is this course partly funded? (y/n): ");
                         bool isPartFunded = fundChoice.ToLower() == "y" ? true : false;
 
-                        List<Subject> courseSubject = makeAssocFromInput("Which subjects are associated with this course?\nSelect using integers delimited by a comma (e.g 1,3,5,7): ", subjects);
-                        List<Student> courseMembership = makeAssocFromInput("Which students are associated with this course?\nSelect using integers delimited by a comma (e.g 1,3,5,7): ", students);
+                        List<Subject> courseSubject = MakeAssocFromInput<Subject>("Which subjects are associated with this course?\nSelect using integers delimited by a comma (e.g 1,3,5,7): ", data, true);
+                        List<Student> courseMembership = MakeAssocFromInput<Student>("Which students are associated with this course?\nSelect using integers delimited by a comma (e.g 1,3,5,7): ", data, false);
 
-                        courses.Add(new Course(courseId, courseName, courseDescription, isPartFunded, courseSubject, courseMembership));
+                        data.Courses.Add(new Course(courseId, courseName, courseDescription, isPartFunded, courseSubject, courseMembership));
                         break;
                     case "6":
                         string subjectId = Guid.NewGuid().ToString();
                         string subjectName = Input("Input subject name: ");
                         string subjectDescription = Input("Input subject description: ");
 
-                        subjects.Add(new Subject(subjectName, subjectDescription, subjectId));
+                        data.Subjects.Add(new Subject(subjectName, subjectDescription, subjectId));
                         break;
                     case "7":
                         // code block
