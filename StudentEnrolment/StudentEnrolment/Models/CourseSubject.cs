e8d98af409982a06ccf86ebb1f98d03de56a60c7
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2022-04-29 18:45:56 +0100

    created 2 models for junction tables

---
+++ StudentEnrolment/StudentEnrolment/Models/CourseSubject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEnrolment.Models
+{
+    public class CourseSubject
+    {
+        [Column(TypeName = "varchar(36)")]
+        [Required(ErrorMessage = "CourseId field is required.")]
+        public string CourseId { get; set; }
+        [Column(TypeName = "varchar(36)")]
+        [Required(ErrorMessage = "SubjectId field is required.")]
+        public string SubjectId { get; set; }
+    }
+}
