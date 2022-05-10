6dfb27f47f72979e39a658c2a8bbafa6f4197034
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2022-05-10 10:53:41 +0100

    added id interface to entity models

--- StudentEnrolment/StudentEnrolment/Models/Student.cs
+++ StudentEnrolment/StudentEnrolment/Models/Student.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudentEnrolment.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace StudentEnrolment.Models
 {
-    public class Student
+    public class Student : IdInterface
     {
         [Column(TypeName = "varchar(36)")]
         [Required(ErrorMessage = "Id field is required.")]
