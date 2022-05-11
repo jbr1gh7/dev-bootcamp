4e7ee68c731ace97052063d69d5a1f1220881a5e
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2022-05-11 17:38:22 +0100

    program.cs boilerplate + CORS

--- StudentEnrolment/StudentEnrolment/Program.cs
+++ StudentEnrolment/StudentEnrolment/Program.cs
@@ -1,7 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using StudentEnrolment.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var _policyName = "CorsPolicy";
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(name: _policyName, builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+var connectionString = builder.Configuration.GetConnectionString(name: "EnrolmentDbContext");
+
+builder.Services.AddDbContext<EnrolmentDbContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 var app = builder.Build();
 
@@ -14,11 +35,19 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseCors(_policyName);
+
 app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.MapRazorPages();
 
