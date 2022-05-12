bfd2b1f28edc8050033057ef5a1b79111d330af4
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2022-05-12 17:55:14 +0100

    added course + cousemembership controller

---
+++ StudentEnrolment/StudentEnrolment/Controllers/CourseController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentEnrolment.Data;
+using StudentEnrolment.Models;
+
+namespace StudentEnrolment.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly EnrolmentDbContext _db;
+        public CourseController(EnrolmentDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("Course/List")]
+        public List<Course> List()
+        {
+            List<Course> itemList = _db.Course.ToList();
+            return itemList;
+        }
+
+        [HttpPost("Course/Create")]
+        public IActionResult Create([FromBody] Course course)
+        {
+            try
+            {
+                _db.Course.Add(course);
+                _db.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost("Course/Delete")]
+        public IActionResult Delete([FromBody] IdBase course)
+        {
+            Course foundCourse = _db.Course.FirstOrDefault(x => x.Id == course.Id);
+            if (foundCourse != null)
+            {
+                try
+                {
+                    _db.Course.Remove(foundCourse);
+                    _db.SaveChanges();
+                    return Ok();
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex);
+                }
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ StudentEnrolment/StudentEnrolment/Controllers/CourseMembershipController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentEnrolment.Data;
+using StudentEnrolment.Models;
+
+namespace StudentEnrolment.Controllers
+{
+    public class CourseMembershipController : Controller
+    {
+        private readonly EnrolmentDbContext _db;
+        public CourseMembershipController(EnrolmentDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("CourseMembership/List")]
+        public List<CourseMembership> List()
+        {
+            List<CourseMembership> itemList = _db.CourseMembership.ToList();
+            return itemList;
+        }
+
+        [HttpPost("CourseMembership/Create")]
+        private IActionResult Create([FromBody] List<CourseMembership> courseMemberships)
+        {
+            try
+            {
+                for (int i = 0; i < courseMemberships.Count; i++)
+                {
+                    _db.CourseMembership.Add(courseMemberships[i]);
+                    _db.SaveChanges();
+                }
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost("CourseMembership/Delete/Student")]
+        private IActionResult DeleteByStudent(Student student)
+        {
+            List<CourseMembership> associations = _db.CourseMembership.Where(x => x.StudentId == student.Id).ToList();
+            return Delete(associations);
+
+        }
+
+        [HttpPost("CourseMembership/Delete/Course")]
+        private IActionResult DeleteByCourse(Course course)
+        {
+            List<CourseMembership> associations = _db.CourseMembership.Where(x => x.CourseId == course.Id).ToList();
+            return Delete(associations);
+        }
+
+        private IActionResult Delete(List<CourseMembership> associations)
+        {
+            if (associations != null)
+            {
+                try
+                {
+                    for (int i = 0; i < associations.Count; i++)
+                    {
+                        _db.CourseMembership.Remove(associations[i]);
+                        _db.SaveChanges();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex);
+                }
+            }
+            return BadRequest();
+        }
+    }
+}
