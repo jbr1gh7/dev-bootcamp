38bebc26db2dce8c2e0dc9a8ab049ce791f83b24
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2022-05-12 17:54:12 +0100

    removed Id interface, uses id base class
    so that I can deserialize id from body argument

---
+++ StudentEnrolment/StudentEnrolment/Models/IdBase.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentEnrolment.Models
+{
+    public class IdBase
+    {
+        [Column(TypeName = "varchar(36)")]
+        [Required(ErrorMessage = "Id field is required.")]
+        public string Id { get; set; }
+    }
+}
