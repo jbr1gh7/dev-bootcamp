931cc0cc5c0f6e5ec85c46cb13094016c52d8dad
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2022-05-20 11:30:51 +0100

    added routing

--- StudentEnrolment/StudentEnrolment/web/src/app/app-routing.module.ts
+++ StudentEnrolment/StudentEnrolment/web/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourseTableComponent } from './components/course-table/course-table.component';
+import { StudentTableComponent } from './components/student-table/student-table.component';
+import { SubjectTableComponent } from './components/subject-table/subject-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'Students', 
+    component: StudentTableComponent
+  },
+  {
+    path: 'Courses', 
+    component: CourseTableComponent
+  },
+  {
+    path: 'Subjects', 
+    component: SubjectTableComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
