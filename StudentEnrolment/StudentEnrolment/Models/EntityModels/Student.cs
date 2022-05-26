2a170183a50c23354facd0f051ae24e392b90064
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2022-05-26 16:20:02 +0100

    moved models to separate files
    also implemented nav properties for db entity models correctly

---
+++ StudentEnrolment/StudentEnrolment/Models/EntityModels/Student.cs
@@ -0,0 +1,24 @@
+﻿using StudentEnrolment.Models.BaseClasses;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentEnrolment.Models.EntityModels
+{
+    public class Student : IdBase
+    {
+        [Column(TypeName = "varchar(40)")]
+        [Required(ErrorMessage = "FirstName field is required.")]
+        public string FirstName { get; set; }
+        [Column(TypeName = "varchar(70)")]
+        [Required(ErrorMessage = "LastName field is required.")]
+        public string LastName { get; set; }
+        public virtual IList<CourseStudent> CourseStudent { get; set; }
+
+        public Student(string id, string firstName, string lastName)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
---
+++ StudentEnrolment/StudentEnrolment/Models/EntityModels/Subject.cs
@@ -0,0 +1,20 @@
+﻿using StudentEnrolment.Models.BaseClasses;
+
+namespace StudentEnrolment.Models.EntityModels
+{
+    public class Subject : Curriculum
+    {
+        public virtual IList<CourseSubject> CourseSubject { get; set; }
+
+        public Subject(
+            string id, 
+            string name, 
+            string description
+        )
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+        }
+    }
+}
