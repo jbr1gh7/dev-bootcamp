5aed7987993a5562be8971d3ba528f09bf0813ad
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2022-05-26 16:16:25 +0100

    added new migrations

---
+++ StudentEnrolment/StudentEnrolment/Migrations/20220526015726_switched_FK_around.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentEnrolment.Migrations
+{
+    public partial class switched_FK_around : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseStudent_Course_StudentId",
+                table: "CourseStudent");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseStudent_Student_CourseId",
+                table: "CourseStudent");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseSubject_Course_SubjectId",
+                table: "CourseSubject");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseSubject_Subject_CourseId",
+                table: "CourseSubject");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseStudent_Course_CourseId",
+                table: "CourseStudent",
+                column: "CourseId",
+                principalTable: "Course",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseStudent_Student_StudentId",
+                table: "CourseStudent",
+                column: "StudentId",
+                principalTable: "Student",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseSubject_Course_CourseId",
+                table: "CourseSubject",
+                column: "CourseId",
+                principalTable: "Course",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseSubject_Subject_SubjectId",
+                table: "CourseSubject",
+                column: "SubjectId",
+                principalTable: "Subject",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseStudent_Course_CourseId",
+                table: "CourseStudent");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseStudent_Student_StudentId",
+                table: "CourseStudent");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseSubject_Course_CourseId",
+                table: "CourseSubject");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseSubject_Subject_SubjectId",
+                table: "CourseSubject");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseStudent_Course_StudentId",
+                table: "CourseStudent",
+                column: "StudentId",
+                principalTable: "Course",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseStudent_Student_CourseId",
+                table: "CourseStudent",
+                column: "CourseId",
+                principalTable: "Student",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseSubject_Course_SubjectId",
+                table: "CourseSubject",
+                column: "SubjectId",
+                principalTable: "Course",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseSubject_Subject_CourseId",
+                table: "CourseSubject",
+                column: "CourseId",
+                principalTable: "Subject",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
