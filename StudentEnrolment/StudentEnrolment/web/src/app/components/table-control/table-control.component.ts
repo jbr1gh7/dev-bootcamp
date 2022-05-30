9598c1df56d1ba58a8e310ea1e09423f3776556a
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2022-05-30 15:40:44 +0100

    table control sends delete call to API

--- StudentEnrolment/StudentEnrolment/web/src/app/components/table-control/table-control.component.ts
+++ StudentEnrolment/StudentEnrolment/web/src/app/components/table-control/table-control.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { EventBusService } from 'src/app/services/event-bus.service';
+import { Router } from '@angular/router';
+import { IdBase } from 'src/app/models/id-base.model';
+import { CourseCrudService } from 'src/app/services/course-crud.service';
+import { StudentCrudService } from 'src/app/services/student-crud.service';
+import { SubjectCrudService } from 'src/app/services/subject-crud.service';
 
 @Component({
   selector: 'app-table-control',
@@ -7,13 +11,50 @@ import { EventBusService } from 'src/app/services/event-bus.service';
   styleUrls: ['./table-control.component.css']
 })
 export class TableControlComponent implements OnInit {
+  deleteList: IdBase[] = [];
 
-  constructor(private eventBus: EventBusService) {
+  constructor(
+    private studentCrud: StudentCrudService,
+    private courseCrud: CourseCrudService,
+    private subjectCrud: SubjectCrudService,
+    private router: Router
+  ) 
+  {
 
   }
 
   ngOnInit(): void {
-
+    this.studentCrud.deleteList.subscribe(list => this.deleteList = list);
+    this.courseCrud.deleteList.subscribe(list => this.deleteList = list);
+    this.subjectCrud.deleteList.subscribe(list => this.deleteList = list);
   }
 
+  add(): void {
+    if (this.router.url == "/Students") {
+      
+    }
+  }
+
+  save(): void {
+    let crud;
+
+    if (this.router.url == "/Students") 
+      crud = this.studentCrud
+    else if (this.router.url == "/Courses") 
+      crud = this.courseCrud
+    else if (this.router.url == "/Subjects") 
+      crud = this.subjectCrud
+    else
+      return;
+
+    crud.delete(this.deleteList).subscribe(
+      (result: any) => {
+        console.log(result);
+        window.location.reload();
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    )
+  }
 }
