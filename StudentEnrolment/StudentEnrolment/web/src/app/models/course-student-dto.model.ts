2a3b89d3fad97c5f2dacf4168a12bd0ae6cee47e
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2022-06-08 17:56:03 +0100

    change typescript model to match .net model

--- StudentEnrolment/StudentEnrolment/web/src/app/models/course-student-dto.model.ts
+++ StudentEnrolment/StudentEnrolment/web/src/app/models/course-student-dto.model.ts
@@ -1,24 +1,13 @@
-import { Course } from "./course.model";
-import { Student } from "./student.model";
-
 export class CourseStudentDto {
     public courseId: string;
-    public course: Course;
-
     public studentId: string | null;
-    public student: Student | null;
 
     constructor(
         courseId: string,
-        course: Course,
         studentId: string | null,
-        student: Student | null
     )
     {
         this.courseId = courseId;
-        this.course = course;
-
         this.studentId = studentId;
-        this.student = student;
     }
 }
